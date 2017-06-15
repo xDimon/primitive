c0656a4394e808c007d6b8076419da35eaa997ac
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2017-06-15 19:35:00 +0300

    Add SipHash calculation class

---
+++ src/utils/hash/SipHash.cpp
@@ -0,0 +1,153 @@
+// Copyright © 2017 Dmitriy Khaustov
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Author: Dmitriy Khaustov aka xDimon
+// Contacts: khaustov.dm@gmail.com
+// File created on: 2017.06.15
+
+// SipHash.cpp
+
+
+#include <cstring>
+#include <algorithm>
+#include "SipHash.hpp"
+
+inline
+static uint64_t rotl(uint64_t x, uint64_t b)
+// Return the bits of the specified 'x' rotated to the left by the
+// specified 'b' number of bits.  Bits that are rotated off the end are
+// wrapped around to the beginning.
+{
+	return (x << b) | (x >> (64 - b));
+}
+
+inline
+static void sipround(uint64_t& v0, uint64_t& v1, uint64_t& v2, uint64_t& v3)
+// Mix the specified 'v0', 'v1', 'v2', and 'v3' in a "Sip Round" as
+// described in the SipHash algorithm
+{
+	v0 += v1;
+	v1 = rotl(v1, 13);
+	v1 ^= v0;
+	v0 = rotl(v0, 32);
+	v2 += v3;
+	v3 = rotl(v3, 16);
+	v3 ^= v2;
+	v0 += v3;
+	v3 = rotl(v3, 21);
+	v3 ^= v0;
+	v2 += v1;
+	v1 = rotl(v1, 17);
+	v1 ^= v2;
+	v2 = rotl(v2, 32);
+}
+
+inline
+static uint64_t u8to64_le(const uint8_t* p)
+// Return the 64-bit integer representation of the specified 'p' taking
+// into account endianness.  Undefined unless 'p' points to at least eight
+// bytes of initialized memory.
+{
+	uint64_t ret;
+	memcpy(&ret, p, sizeof(ret));
+	return le64toh(ret);
+}
+
+
+SipHash::SipHash(const char (&seed)[16])
+	: d_v0(0x736f6d6570736575ULL)
+	, d_v1(0x646f72616e646f6dULL)
+	, d_v2(0x6c7967656e657261ULL)
+	, d_v3(0x7465646279746573ULL)
+	, d_bufSize(0)
+	, d_totalLength(0)
+{
+	uint64_t k0 = u8to64_le(reinterpret_cast<const uint8_t*>(&seed[0]));
+	uint64_t k1 = u8to64_le(reinterpret_cast<const uint8_t*>(&seed[8]));
+
+	d_v0 ^= k0;
+	d_v1 ^= k1;
+	d_v2 ^= k0;
+	d_v3 ^= k1;
+}
+
+void
+SipHash::operator()(const void* data, size_t numBytes)
+{
+	uint8_t const* in = static_cast<const uint8_t*>(data);
+
+	d_totalLength += numBytes;
+	if (d_bufSize + numBytes < 8)
+	{
+		std::copy(in, in + numBytes, d_buf + d_bufSize);
+		d_bufSize += numBytes;
+		return;                                                       // RETURN
+	}
+	if (d_bufSize > 0)
+	{
+		size_t t = 8 - d_bufSize;
+		std::copy(in, in + t, d_buf + d_bufSize);
+		uint64_t m = u8to64_le(d_buf);
+		d_v3 ^= m;
+		sipround(d_v0, d_v1, d_v2, d_v3);
+		sipround(d_v0, d_v1, d_v2, d_v3);
+		d_v0 ^= m;
+		in += t;
+		numBytes -= t;
+	}
+	d_bufSize = numBytes & 7;
+	uint8_t const* const end = in + (numBytes - d_bufSize);
+	for (; in != end; in += 8)
+	{
+		uint64_t m = u8to64_le(in);
+		d_v3 ^= m;
+		sipround(d_v0, d_v1, d_v2, d_v3);
+		sipround(d_v0, d_v1, d_v2, d_v3);
+		d_v0 ^= m;
+	}
+	std::copy(end, end + d_bufSize, d_buf);
+}
+
+SipHash::result_type SipHash::computeHash()
+{
+	// The "FALL THROUGH" comments here are necessary to avoid the
+	// implicit-fallthrough warnings that GCC 7 introduces.  We could
+	// instead use GNU C's __attribute__(fallthrough) vendor
+	// extension or C++17's [[fallthrough]] attribute but these would
+	// need to be hidden from the Oracle and IBM compilers.
+
+	result_type b = static_cast<uint64_t>(d_totalLength) << 56;
+	switch (d_bufSize)
+	{
+		case 7:b |= static_cast<uint64_t>(d_buf[6]) << 48;  // FALL THROUGH
+		case 6:b |= static_cast<uint64_t>(d_buf[5]) << 40;  // FALL THROUGH
+		case 5:b |= static_cast<uint64_t>(d_buf[4]) << 32;  // FALL THROUGH
+		case 4:b |= static_cast<uint64_t>(d_buf[3]) << 24;  // FALL THROUGH
+		case 3:b |= static_cast<uint64_t>(d_buf[2]) << 16;  // FALL THROUGH
+		case 2:b |= static_cast<uint64_t>(d_buf[1]) << 8;   // FALL THROUGH
+		case 1:b |= static_cast<uint64_t>(d_buf[0]);        // FALL THROUGH
+		case 0:break;
+	}
+	d_v3 ^= b;
+	sipround(d_v0, d_v1, d_v2, d_v3);
+	sipround(d_v0, d_v1, d_v2, d_v3);
+	d_v0 ^= b;
+	d_v2 ^= 0xff;
+	sipround(d_v0, d_v1, d_v2, d_v3);
+	sipround(d_v0, d_v1, d_v2, d_v3);
+	sipround(d_v0, d_v1, d_v2, d_v3);
+	sipround(d_v0, d_v1, d_v2, d_v3);
+	b = d_v0 ^ d_v1 ^ d_v2 ^ d_v3;
+	return b;
+}
---
+++ src/utils/hash/SipHash.hpp
@@ -0,0 +1,88 @@
+// Copyright © 2017 Dmitriy Khaustov
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Author: Dmitriy Khaustov aka xDimon
+// Contacts: khaustov.dm@gmail.com
+// File created on: 2017.06.15
+
+// SipHash.hpp
+
+
+#pragma once
+
+
+#include <cstdint>
+#include <cstddef>
+
+class SipHash
+{
+private:
+	// DATA
+	uint64_t d_v0;
+	uint64_t d_v1;
+	uint64_t d_v2;
+	uint64_t d_v3;
+
+	// Stores the intermediate state of the algorithm as values areaccumulated
+	union
+	{
+		uint64_t d_alignment; // Provides alignment
+		unsigned char d_buf[8];	// Used to buffer data until we have enough to do a full round of computation as specified by the algorithm.
+	};
+
+	size_t d_bufSize; // The length of the data currently stored in the buffer.
+
+	size_t d_totalLength; // The total length of all data that has been passed into the algorithm.
+
+	SipHash(const SipHash& original) = delete; // Do not allow copy construction.
+
+	SipHash& operator=(const SipHash& rhs) = delete; // Do not allow assignment.
+
+public:
+	// TYPES
+	typedef uint64_t result_type; // Typedef indicating the value type returned by this algorithm.
+
+	// CREATORS
+	explicit SipHash(const char (&seed)[16]);
+	// Create a 'SipHash', seeded with a 128-bit
+	// ('k_SEED_LENGTH' bytes) seed pointed to by the specified 'seed'.
+	// Each bit of the supplied seed will contribute to the final hash
+	// produced by 'computeHash()'.  The behaviour is undefined unless
+	// 'seed' points to at least 16 bytes of initialized memory.  Note that
+	// if data in 'seed' is not random, all guarantees of security and
+	// Denial of Service (DoS) protection are void.
+
+	~SipHash() = default;
+
+	// MANIPULATORS
+	void operator()(const void* data, size_t numBytes);
+	// Incorporate the specified 'data', of at least the specified
+	// 'numBytes', into the internal state of the hashing algorithm.  Every
+	// bit of data incorporated into the internal state of the algorithm
+	// will contribute to the final hash produced by 'computeHash()'.  The
+	// same hash will be produced regardless of whether a sequence of bytes
+	// is passed in all at once or through multiple calls to this member
+	// function.  Input where 'numBytes' is 0 will have no effect on the
+	// internal state of the algorithm.  The behaviour is undefined unless
+	// 'data' points to a valid memory location with at least 'numBytes'
+	// bytes of initialized memory.
+
+	result_type computeHash();
+	// Return the finalized version of the hash that has been accumulated.
+	// Note that this changes the internal state of the object, so calling
+	// 'computeHash()' multiple times in a row will return different
+	// results, and only the first result returned will match the expected
+	// result of the algorithm.  Also note that a value will be returned,
+	// even if data has not been passed into 'operator()'
+};
