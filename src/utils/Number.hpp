452bb268ee16e545103d7e6fee09e46794d30772
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2017-07-06 19:52:25 +0300

    Add util for assing numbers with aligns

---
+++ src/utils/Number.hpp
@@ -0,0 +1,42 @@
+// Copyright © 2017 Dmitriy Khaustov
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Author: Dmitriy Khaustov aka xDimon
+// Contacts: khaustov.dm@gmail.com
+// File created on: 2017.07.06
+
+// Number.hpp
+
+
+#pragma once
+
+
+namespace Number
+{
+	template<typename D, typename S>
+	void assign(D& dst, const S& src, const D& def = D())
+	{
+		if (
+			src > std::numeric_limits<D>::max() ||
+			src < std::numeric_limits<D>::min()
+		)
+		{
+			dst = 0;
+		}
+		else
+		{
+			dst = static_cast<D>(src);
+		}
+	}
+};
