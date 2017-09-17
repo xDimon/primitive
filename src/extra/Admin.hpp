878a1b8afe17d535e16f7b13ac081b01ac510bf1
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2017-09-17 22:11:02 +0300

    Add Admin service part

---
+++ src/extra/Admin.hpp
@@ -0,0 +1,39 @@
+// Copyright © 2017 Dmitriy Khaustov
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Author: Dmitriy Khaustov aka xDimon
+// Contacts: khaustov.dm@gmail.com
+// File created on: 2017.09.17
+
+// Admin.hpp
+
+
+#pragma once
+
+
+#include "../../src/extra/Application.hpp"
+
+class Admin: public Application
+{
+private:
+	std::string _appKey;
+
+public:
+	const std::string& appKey() const
+	{
+		return _appKey;
+	}
+
+DECLARE_APPLICATION(Admin);
+};
