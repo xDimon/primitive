52cc1035fdf351a5a47c72bf601580d849b313e2
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2018-01-19 23:31:09 +0300

    Add DoAtExitScope class

---
+++ src/utils/DoAtExitScope.hpp
@@ -0,0 +1,47 @@
+// Copyright © 2018 Dmitriy Khaustov
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Author: Dmitriy Khaustov aka xDimon
+// Contacts: khaustov.dm@gmail.com
+// File created on: 2018.01.13
+
+// DoAtExitScope.hpp
+
+
+#pragma once
+
+#include <functional>
+
+class DoAtExitScope
+{
+private:
+	std::function<void()> _func;
+
+public:
+	DoAtExitScope(std::function<void()>&& func)
+	: _func(std::move(func))
+	{
+	}
+
+	DoAtExitScope() = delete; // Copy-constructor
+	DoAtExitScope(const DoAtExitScope&) = delete; // Copy-constructor
+	void operator=(DoAtExitScope const&) = delete; // Copy-assignment
+	DoAtExitScope(DoAtExitScope&&) = delete; // Move-constructor
+	DoAtExitScope& operator=(DoAtExitScope&&) = delete; // Move-assignment
+
+	~DoAtExitScope()
+	{
+		_func();
+	}
+};
